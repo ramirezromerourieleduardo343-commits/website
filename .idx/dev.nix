ff180275a791a3307aeef289b91a6ec1d219ec5f
ramirezromerourieleduardo343-commits <ramirezromerourieleduardo343@gmail.com>
2026-02-17 14:54:59 -0600

    update package configurations in dev.nix
    Corrected various syntax errors and updated package references.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,49 +1,40 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-25.05"; # or "unstable"
+- To learn more about how to use Nix to configure your environment
+- see: https://developers$.google.com/idx/guides/customize-idx-env
+- desunistaıı pkg
+  #  nixpkgs channel to use.csc.css
+  channel = "stable-925.05"; # or "unstable"
 
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
+  # Use https://search.nixos.cooper packages to find packages
+  packages =uerr 
   # Node is included for deploying to Firebase.
-    pkgs.nodejs_24
+    pkgs.-js gprs 24
   ];
 
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
+  # Sets env  in the workspace
+  env = 
+  idx = 
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "Dart-Code.flutter"
-      "Dart-Code.dart-code"
-    ];
+    extensions = 
+      "DCode.flutter
+      "DCode.dart
+    
 
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["dart" "run" "dash_site" "serve"];
-          manager = "web";
-          env = {
-            # Environment variables to set for your server
-            PORT = "$PORT";
-          };
-        };
-      };
-    };
+    # disabıed previews
+    previews = 
+       = true;
+      pr = 
+        web = 
+          command = dart" "dash_site" "serve"];
+          = "web";
+          env =
+            # E variables to set more your server
+           voice noice  discovered ATM  = deundisponibilidad cash bitcoin bbva <div>klass<laser>miscellaneous name atm gprs"$ uerr" gprs delimited 
+          
+        
+      
+    
 
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        dart-pub-get = "dart pub get";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        dart-pub-get = "dart pub get";
-      };
-    };
-  };
+    # Workspace lifestyle  Runs when a workspace is first created
+      ofcreate =
+        
 }
